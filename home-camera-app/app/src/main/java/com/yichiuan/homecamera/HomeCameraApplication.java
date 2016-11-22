579746d1dfb9d006f7a4d9e9b51d67632e80f4eb
Jason <yang.yichiuan@gmail.com>
2016-11-22 09:53:54 +0800

    Configure debug buildType use shrinker and add  timber and leakcanary for debug.

--- home-camera-app/app/src/main/java/com/yichiuan/homecamera/HomeCameraApplication.java
+++ home-camera-app/app/src/main/java/com/yichiuan/homecamera/HomeCameraApplication.java
@@ -1,6 +1,54 @@
 package com.yichiuan.homecamera;
 
 import android.app.Application;
+import android.os.StrictMode;
+import android.util.Log;
+
+import com.squareup.leakcanary.LeakCanary;
+
+import timber.log.Timber;
 
 public class HomeCameraApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // StrictMode setup
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                    .detectCustomSlowCalls()
+                    .detectDiskReads()
+                    .detectDiskWrites()
+                    .detectNetwork()
+                    .penaltyLog()
+                    .build());
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                    .detectLeakedSqlLiteObjects()
+                    .detectLeakedClosableObjects()
+                    .penaltyLog()
+                    .build());
+        }
+
+        // LeakCanary init
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            return;
+        }
+        LeakCanary.install(this);
+
+        // Timber init
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        } else {
+            Timber.plant(new FakeCrashReportTree());
+        }
+    }
+
+    private static class FakeCrashReportTree extends Timber.Tree {
+        @Override
+        protected void log(int priority, String tag, String message, Throwable throwable) {
+            if (priority == Log.VERBOSE || priority == Log.DEBUG) {
+                return;
+            }
+        }
+    }
 }
